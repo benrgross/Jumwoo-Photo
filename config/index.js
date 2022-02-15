0f402d676bc8a523f2645261463c7160a6bd4560
Ben Gross <benrgross@gmail.com>
2022-02-15 09:22:31 -0500

    addign new domain

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://jumwoo-photo-3dg4cdo51-benrgross.vercel.app";
+  : "https://jumwoo-photos.vercel.app";
